ff2c384ade9ce96556a80337c3e62312d146ac47
Ankit Aggarwal <ankit.spd@gmail.com>
2016-06-01 12:14:07 +0530

    Dev preview parsing

--- banana.rb
+++ banana.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 require 'open-uri'
 require "json"
 
-def fetchLatestSnapshots
+def fetchSwiftOrg link_idx, date_idx
     doc = Nokogiri(open("https://swift.org/download/#snapshots"))
     # FIXME: This is unrealible and will break in future ;D
     rows = doc.xpath('//body').xpath('//table[@id="latest-builds"]/tbody/tr')
@@ -12,9 +12,10 @@ def fetchLatestSnapshots
 
     value = lambda { |link, date| return {:url => "https://swift.org" + link.attribute("href").value, :time => date.attribute("datetime").value } }
 
-    xcode = value.call links[10], date[6]
-    ubuntu1510 = value.call links[12], date[7]
-    ubuntu1404 = value.call links[14], date[8]
+    puts date
+    xcode = value.call links[link_idx], date[date_idx]
+    ubuntu1510 = value.call links[link_idx+2], date[date_idx+1]
+    ubuntu1404 = value.call links[link_idx+4], date[date_idx+2]
 
     data = []
     data.push(makeSnapshotHash "Xcode", xcode)
@@ -28,6 +29,14 @@ end
 
 def makeSnapshotHash name, data
     {:title => name, :value => "<#{data[:url]}|Download>", :short => true}
+end
+
+def fetchSnapshot
+    fetchSwiftOrg 10, 6
+end
+
+def devPreview
+    fetchSwiftOrg 76, 9
 end
 
 class BananaBot < SlackRubyBot::Bot
@@ -44,11 +53,7 @@ class BananaBot < SlackRubyBot::Bot
         client.say(text: "Hello @#{match[:user]}, hope you\'re doing good today!", channel: data.channel)
     end
 
-    command 'snapshot' do |client, data, match|
-        client.web_client.chat_postMessage(
-            channel: data.channel, text: 'Hold on fetching lastest snapshot info...', as_user: true)
-
-        snapshot = fetchLatestSnapshots
+    def self.postSnapshotInfo client, data, snapshot, title
         client.web_client.chat_postMessage(
             channel: data.channel,
             as_user: true,
@@ -56,7 +61,7 @@ class BananaBot < SlackRubyBot::Bot
                 {
                     "fallback": snapshot[:name],
                     "color": "#36a64f",
-                    "title": "Swift Trunk Snapshots",
+                    "title": title,
                     "text": "*Released on #{snapshot[:date]}* `#{snapshot[:name]}`",
                     "title_link": "https://swift.org/download/#releases",
                     "mrkdwn_in": ["text"],
@@ -66,6 +71,21 @@ class BananaBot < SlackRubyBot::Bot
         )
     end
 
+    command 'snapshot' do |client, data, match|
+        client.web_client.chat_postMessage(
+            channel: data.channel, text: 'Hold on fetching lastest snapshot info...', as_user: true)
+
+        self.postSnapshotInfo client, data, fetchSnapshot, "Swift Trunk Snapshot" 
+    end
+
+    command 'preview' do |client, data, match| 
+        client.web_client.chat_postMessage(
+            channel: data.channel, text: 'Hold on fetching lastest preview info...', as_user: true)
+
+        postSnapshotInfo client, data, devPreview, "Swift Developer Preview" 
+    end
+
+    
     command 'pulls swiftpm' do |client, data, match| 
         json = JSON.parse(open("https://api.github.com/repos/apple/swift-package-manager/pulls?sort=updated&direction=desc").read)
         count = json.count
